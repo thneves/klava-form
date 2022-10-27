86ed330a94da01e8c530d2ba10e937492d9b5eea
thales-pinner <thales.neves@pinpeople.com.br>
2022-10-26 22:53:38 -0300

    Delete default assets, add bootstrap form

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react"
+import { useForm } from 'react-hook-form';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './App.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const {Group, Label, Control} = {...Form}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Add Klaviyo Profile</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="simpleForm">
+      <Group>
+        <Label>
+          First Name
+        </Label>
+        <Control type="text" placeholder="First name" {...register("First name")} />
+      </Group>
+
+      <Group>
+        <Label>Last Name</Label>
+        <Control type="text" placeholder="Last name" {...register("Last name")} />
+      </Group>
+      <Group>
+        <Label>Email</Label>
+        <Control type="text" placeholder="Email" {...register("Email")} />
+      </Group>
+      <Group>
+        <Label>Phone Number</Label>
+        <Control  type="tel" placeholder="Phone Number" {...register("Phone Number")} />
+      </Group>
+      <Group>
+        <Label>Date of Birth</Label>
+        <Control type="date" {...register("Date of Birth")} />
+
+        <Button type="submit" >
+          Send
+        </Button>
+      </Group>
+    </form>
     </div>
   )
 }
