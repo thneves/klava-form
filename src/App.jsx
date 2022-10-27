609ed75f85ccf6fc11d8d9db05bd06c78841ca10
thales-pinner <thales.neves@pinpeople.com.br>
2022-10-27 09:07:43 -0300

    Add custom errors to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,146 @@
+import React from 'react';
 import { useState } from "react"
 import { useForm } from 'react-hook-form';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { ErrorMessage } from '@hookform/error-message';
 import './App.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const {Group, Label, Control} = {...Form}
 
 function App() {
-
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [date, setDate] = useState(new Date());
+  const { register, handleSubmit,  formState: { errors } } = useForm({
+    criteriaMode: "all"
+  });
   const onSubmit = data => console.log(data);
-  console.log(errors);
-
+  console.log(errors)
   return (
-    <div className="App">
-      <div>
-      </div>
-      <h1>Add Klaviyo Profile</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="simpleForm">
-      <Group>
-        <Label>
-          First Name
-        </Label>
-        <Control type="text" placeholder="First name" {...register("First name")} />
-      </Group>
+    <>
+    
+      <h1 className="title">Add Profile</h1>
+      <form className='form' onSubmit={handleSubmit(onSubmit)}>
+      <input
+        placeholder='First Name'
+        {...register("FirstName", {
+          required: "This is required.",
+          pattern: {
+            value: /^[a-zA-Z]+$/,
+            message: "This input is letters only."
+          },
+          maxLength: {
+            value: 22,
+            message: "This input exceed maxLength."
+          }
+        })}
+      />
+      <ErrorMessage
+        errors={errors}
+        name="FirstName"
+        render={({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      />
+           <input
+        placeholder='Second Name'
+        {...register("SecondName", {
+          required: "This is required.",
+          pattern: {
+            value: /^[a-zA-Z]+$/,
+            message: "This input is letters only."
+          },
+          maxLength: {
+            value: 22,
+            message: "This input exceed maxLength."
+          }
+        })}
+      />
+      <ErrorMessage
+        errors={errors}
+        name="SecondName"
+        render={({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      />
 
-      <Group>
-        <Label>Last Name</Label>
-        <Control type="text" placeholder="Last name" {...register("Last name")} />
-      </Group>
-      <Group>
-        <Label>Email</Label>
-        <Control type="text" placeholder="Email" {...register("Email")} />
-      </Group>
-      <Group>
-        <Label>Phone Number</Label>
-        <Control  type="tel" placeholder="Phone Number" {...register("Phone Number")} />
-      </Group>
-      <Group>
-        <Label>Date of Birth</Label>
-        <Control type="date" {...register("Date of Birth")} />
+      <input
+        placeholder='Email Address'
+        {...register("Email", {
+          required: "This is required.",
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: "Must be a valid email."
+          }
+        })}
+      />
+      <ErrorMessage
+        errors={errors}
+        name="Email"
+        render={({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      />
 
-        <Button type="submit" >
-          Send
-        </Button>
-      </Group>
+           <input
+        placeholder='Phone Number'
+        {...register("Phone", {
+          required: "This is required.",
+          pattern: {
+            value: /^[0-9+-]+$/,
+            message: "This input is numbers only."
+          },
+          minLength: {
+            value: 6,
+            message: "Number is too short."
+          },
+          maxLength: {
+            value: 12,
+            message: "Number is too long."
+          }
+        })}
+      />
+      <ErrorMessage
+        errors={errors}
+        name="Phone"
+        render={({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      />
+
+        <input
+        className='date'
+        type="date"
+        placeholder='Date of Birth'
+        {...register("Birth", {
+          required: "This is required.",
+        })}
+      />
+      <ErrorMessage
+        errors={errors}
+        name="Birth"
+        render={({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      />
+      
+      <input value="Submit" type="submit" />
+
     </form>
-    </div>
+    </>
   )
 }
 
